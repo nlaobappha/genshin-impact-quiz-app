33ebb5ee28936f39c2d42efc82812942d6301d13
nlaobappha <114454714+nlaobappha@users.noreply.github.com>
2022-12-08 22:01:27 -0500

    changed to previous ver

--- scripts.js
+++ scripts.js
@@ -1,338 +1,166 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
+let shuffledQuestions, currentQuestionIndex
 
-
-
-
-const header = document.querySelector('.headline')
-const form = document.querySelector('.form')
-const hamburger = document.querySelector('.hamburger')
-const hamdiv = document.querySelector('.hamburger div')
-const options = form.querySelectorAll('.option')
-const answers = document.querySelectorAll('.ans')
-const buttons = document.querySelectorAll('button')
-const question = document.querySelector('.que')
-const alertBox = document.querySelector('.alert__box')
-const alertBoxScore = document.querySelector('.alert__box span')
-const score = document.querySelector('.score')
-const totalScore = document.querySelector(".total__score")
-const themeSelector = document.querySelector('.theme_mode')
-const   installBtn=document.querySelector('.install__btn')
-
-var axios;
-
-// let xhttp = new XMLHttpRequest()
-let x;
-let index = 0;
-let result = 0;
-score.textContent = index
-totalScore.textContent = result
-alertBox.style.display = "none"
-const arrlength = [0, 1, 2, 3]
-arrlength.sort((a, b) => { return 0.5 - Math.random() })
-hamburger.addEventListener('mouseover', hamFunction)
-hamburger.addEventListener('mouseout', hamFunction1)
-let correctAnswer;
-let submitting = false;
-(function () {
-    localStorage.setItem('ispwainstalled',false)
-    const value = localStorage.getItem('theme')
-    if (value === 'Dark Mode') {
-        darkMode()
-    }
-    else if (value === 'Light Mode') {
-        return null
-    }
-    else {
-        localStorage.setItem('theme', 'Light Mode')
-    }
-})()
-
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('sw.js')
-      .then(() => { console.log('Service Worker Registered'); });
-  }
-
-
-const evalFunc = async function () {
-
-    // if (xhttp.status === 200) {
-        form.style.display = "block"
-        toggleLoading()
-
-        options.forEach(item => {
-            item.checked = false;
-        })
- 
-        if (x.results[index].incorrect_answers.length === 3) {
-            answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-            answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-            answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-            answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-            question.innerHTML = x.results[index].question
-            correctAnswer = x.results[index].correct_answer;
-
-        }
-        else {
-            index++;
-            answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-            answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-            answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-            answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-            question.innerHTML = x.results[index].question
-            correctAnswer = x.results[index].correct_answer;
-
-
-        }
-    // }
-}
-
-// xhttp.onload = function() {
-//     x = JSON.parse(this.response)
-//     console.log(x);
-//     evalFunc();
-// }
-// xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
-// xhttp.send()
-
-async function getData() {
-    const response = await axios.get(`https://opentdb.com/api.php?amount=10`);
-    return response;
-}
-
-(async() => {
-    try {
-        const result = await getData();   
-        x = result.data;
-        evalFunc();
-    } catch (err) {
-        console.log(err);
-    }
-})();
-
-
-
-
-const onHandleSubmit = () => {
-    var count = 0;
-    if (submitting) {
-        return; // ignore if the user has already submitted the answer for this question
-    }
-    (options.forEach((item) => {
-        if (item.checked) {
-            submitting = true;
-            if (item.value === options[arrlength[0]].value) {
-                var timer = 2;
-                document.getElementById('timer').innerHTML = timer;
-                document.getElementById('question_switch_info').style.visibility = "visible";
-                var nextQuestionTimer = setInterval(function() {
-                    timer -= 1;
-                    if(timer <= 0) {
-                        clearInterval(nextQuestionTimer);
-                        document.getElementById('question_switch_info').style.visibility = "hidden";
-                    }
-                    document.getElementById('timer').innerHTML = timer;
-                }, 1000)
-              
-                let correctAnswerId = options[arrlength[0]].value + 'a';
-                showRightAnswer(correctAnswerId);
-                function showRightAnswer() {
-                    document.getElementById(options[arrlength[0]].value + 'a').classList.add('correct-answer');
-                    setTimeout(() => {
-                        form.style.display = "none"
-                        index++
-                        result++
-                        evalFunc();
-                        score.textContent = index
-                        totalScore.textContent = result
-                        document.getElementById(correctAnswerId).classList.remove('correct-answer');
-                    }, 2000);
-                }
-            }
-            else {
-                var timer = 2;
-                document.getElementById('timer').innerHTML = timer;
-                document.getElementById('question_switch_info').style.visibility = "visible";
-                var nextQuestionTimer = setInterval(function() {
-                    timer -= 1;
-                    if(timer <= 0) {
-                        clearInterval(nextQuestionTimer);
-                        document.getElementById('question_switch_info').style.visibility = "hidden";
-                    }
-                    document.getElementById('timer').innerHTML = timer;
-                }, 1000)
-              
-                let correctAnswerId = options[arrlength[0]].value + 'a';
-                let wrongAnswerId = item.value + 'a';
-                showRightAndWrongAnswer();
-                function showRightAndWrongAnswer() {
-                    document.getElementById(correctAnswerId).classList.add('correct-answer');
-                    document.getElementById(wrongAnswerId).classList.add('wrong-answer');
-                    setTimeout(() => {
-                        index++
-                        evalFunc();
-                        score.textContent = index
-                        document.getElementById(correctAnswerId).classList.remove('correct-answer');
-                        document.getElementById(wrongAnswerId).classList.remove('wrong-answer');
-                    }, 2000);
-                }
-            }
-            setTimeout(() => {
-                toggleLoading()
-                submitting = false;
-            }, 2000)
-
-            arrlength.sort((a, b) => { return 0.5 - Math.random() })
-
-            if (index === 10) {
-                alertBox.style.display = "block"
-                alertBoxScore.textContent = result
-
-            }
-
-        }
-        else {
-            count++;
-        }
-
-    }))
-    if (count == 4) {
-        alert("Please select an option!");
-    }
-
-}
-
-const toggleLoading = () => {
-    document.querySelector(".form").classList.toggle("hide");
-    document.querySelector(".form").classList.toggle("show");
-    document.querySelector(".lds-ring").classList.toggle("hide");
-    document.querySelector(".lds-ring").classList.toggle("show");
-
-}
-
-function hamFunction() {
-
-    hamdiv.classList.add('blackhover')
-}
-
-function hamFunction1() {
-
-    hamdiv.classList.remove('blackhover')
-}
-
-function onHandleReset() {
-    index = 0;
-    result = 0;
-    toggleLoading()
-
-    form.style.display = "none"
-    // xhttp.onload = function () {
-
-        // if (xhttp.status)
-
-            // if (xhttp.status === 200) {
-
-                async function getData() {
-                    const response = await axios.get(`https://opentdb.com/api.php?amount=10`);
-                    return response;
-                }
-                
-                (async() => {
-                    try {
-                        const result = await getData();   
-                        x = result.data;
-                    
-                    } catch (err) {
-                        console.log(err);
-                    }
-                })();
-
-                setTimeout(() => {
-                    document.querySelector(".lds-ring").classList.add("hide");
-                    form.style.display = "block"
-                    form.classList.remove('hide')
-                    form.classList.add('show')
-                }, 2000)
-
-                options.forEach(item => {
-                    item.checked = false;
-                })
-
-                // x = JSON.parse(this.response)
-                if (x.results[index].incorrect_answers.length === 3) {
-                    answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-                    answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-                    answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-                    answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-                    question.innerHTML = x.results[index].question
-                    correctAnswer = x.results[index].correct_answer;
-
-                }
-                else {
-                    index++;
-                    answers[arrlength[0]].innerHTML = x.results[index].correct_answer
-                    answers[arrlength[1]].innerHTML = x.results[index].incorrect_answers[0]
-                    answers[arrlength[2]].innerHTML = x.results[index].incorrect_answers[1]
-                    answers[arrlength[3]].innerHTML = x.results[index].incorrect_answers[2]
-                    question.innerHTML = x.results[index].question
-                    correctAnswer = x.results[index].correct_answer;
-
-
-                }
-            // }
-    // }
-    // xhttp.open('GET', "https://opentdb.com/api.php?amount=10", true)
-    // xhttp.send()
-
- 
-
-    score.textContent = index
-    totalScore.textContent = result
-    alertBox.style.display = "none"
-}
-
-function darkMode() {
-    document.body.classList.toggle("dark");
-    document.querySelector('.right_btn')
-    document.querySelector('.left_btn')
-    document.querySelector('.container')
-
-    localStorage.setItem('theme', themeSelector.textContent)
-    const value = localStorage.getItem('theme')
-
-
-    value === 'Dark Mode' ? themeSelector.textContent = "Light Mode" : themeSelector.textContent = "Dark Mode"
-    document.querySelector(".form").classList.toggle("dark");
-}
-
-
-
-let deferredPromt
-installBtn.style.display='none'
-// console.log(!localStorage.getItem('ispwainstalled'))
-
-    window.addEventListener('beforeinstallprompt',(e)=>{
-        e.preventDefault()
-        deferredPromt=e
-        installBtn.style.display="block"
-    })
-    
-
-installBtn.addEventListener("click",(e)=>{
-    e.target.style.display="none"
-    deferredPromt.prompt()
-    console.log('localstorage   access')
-    deferredPromt.userChoice.then((choiceResult)=>{
-        if(choiceResult.content==='accepted'){
-        console.log('user accepted to install pwa')
-        localStorage.setItem('ispwainstalled',true)
-}
-        else{
-        console.log('user declined to install pwa')
-    localStorage.setItem('ispwainstalled',false)
-    }
-    })
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
 })
 
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
 
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'Who is the Anemo archon?',
+    answers: [
+      { text: 'Zhongli', correct: false },
+      { text: 'Venti', correct: true },
+      { text: 'Ei', correct: false },
+      { text: 'Nahida', correct: false }
+    ]
+  },
+  {
+    question: 'What are the names of the twins we play as?',
+    answers: [
+      { text: 'Spring and Autumn', correct: false },
+      { text: 'Luna and Eclipse', correct: false },
+      { text: 'Aether and Lumine', correct: true },
+      { text: 'Aster and Dandelion', correct: false }
+    ]
+  },
+  {
+    question: 'How many Yakshas are appointed by Rex Lapis?',
+    answers: [
+      { text: '3', correct: false },
+      { text: '4', correct: false },
+      { text: '5', correct: true },
+      { text: '7', correct: false }
+    ]
+  },
+  {
+    question: 'Yae Miko is an idol from Mondstadt.',
+    answers: [
+      { text: 'TRUE', correct: false },
+      { text: 'FALSE', correct: true }
+    ]
+  },
+  {
+    question: 'How many nations are in the world of Teyvat?',
+    answers: [
+      { text: '7', correct: true },
+      { text: '10', correct: false },
+      { text: '15', correct: false },
+      { text: '16', correct: false}
+    ]
+  },
+  {
+    question: 'Arataki Itto is the leader and founder of',
+    answers: [
+      { text: 'Ox Crew', correct: false },
+      { text: 'Demon Rights Group', correct: false },
+      { text: 'Beetle Fight Club', correct: false },
+      { text: 'Arataki Gang', correct: true }
+    ]
+  },
+  {
+    question: 'You can harvest Dandelion Seed by using what element?',
+    answers: [
+      { text: 'Anemo', correct: true },
+      { text: 'Electro', correct: false },
+      { text: 'Cryo', correct: false },
+      { text: 'Dendro', correct: false }
+    ]
+  },
+  {
+    question: 'The Raiden Shogun have a twin sister.',
+    answers: [
+      { text: 'TRUE', correct: true },
+      { text: 'FALSE', correct: false }
+    ]
+  },
+  {
+    question: 'What rank is Dottore out of the eleven Fatui Harbingers?',
+    answers: [
+      { text: '1', correct: false },
+      { text: '2', correct: true },
+      { text: '4', correct: false },
+      { text: '8', correct: false }
+    ]
+  },
+  {
+    question: 'Why do people like playing Genshin Impact?',
+    answers: [
+      { text: 'The compelling story', correct: true },
+      { text: 'The fun gameplay', correct: true },
+      { text: 'The amazing music', correct: true },
+      { text: 'The endless exploration', correct: true }
+    ]
+  }
+]
